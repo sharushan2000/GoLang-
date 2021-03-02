831a262d366551d30acb3e1357f50b8893ceb146
sharushan2000 <sharushan0000@gmail.com>
2021-03-02 09:09:00 +0530

    Variables

---
+++ Variables.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	var number int = 44
+	var name string = "Horrid"
+	//var dec float64 = 13.222
+	//var state bool = true
+
+	//number := 44
+	//name := "Horrid"
+	dec := 13.222
+	state := false
+
+	fmt.Println(number)
+	fmt.Println(name)
+	fmt.Println(dec)
+	fmt.Println(state)
+
+}
