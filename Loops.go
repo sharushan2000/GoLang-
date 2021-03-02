74510ca6bad34ae70ea727db50d12bc248636d11
sharushan2000 <sharushan0000@gmail.com>
2021-03-02 11:21:32 +0530

    Loops

---
+++ Loops.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"	"go/printer"
+)
+
+
+func main() {
+	/* x := 1
+	for x < 5 {
+		x++
+		fmt.Println(x)
+	} */
+
+	 for x := 0; x <= 5; x++ {
+		// variable ; ,condition ; , increment
+		fmt.Println(x)
+
+	} 
+	
+
+}
